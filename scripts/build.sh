4fee5af97dc963ac9f0bd5c9a42e1842f45f0b88
Phil Winder <phil@winderresearch.com>
2016-08-11 13:00:28 +0100

    Initial commit.

---
+++ scripts/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -ev
+
+SCRIPT_DIR=$(dirname "$0")
+
+if [[ -z "$GROUP" ]] ; then
+    echo "Cannot find GROUP env var"
+    exit 1
+fi
+
+if [[ -z "$COMMIT" ]] ; then
+    echo "Cannot find COMMIT env var"
+    exit 1
+fi
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    DOCKER_CMD=docker
+else
+    DOCKER_CMD="sudo docker"
+fi
+CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
+echo $CODE_DIR
+$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
+
+cp $CODE_DIR/target/*.jar $CODE_DIR/docker/$(basename $CODE_DIR)
+
+for m in ./docker/*/; do
+    REPO=${GROUP}/$(basename $m)
+    $DOCKER_CMD build -t ${REPO}:${COMMIT} $CODE_DIR/$m;
+done;
