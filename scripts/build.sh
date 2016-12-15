ad67916128421c239ddddd5a961ee34955ba015f
Vishal Lal <vishal.lal@container-solutions.com>
2016-12-15 14:59:38 -0800

    remove force flag

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,8 +25,8 @@ CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 $DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
 
-cp -rf $CODE_DIR/docker $CODE_DIR/target/docker/ 
-cp -rf $CODE_DIR/target/*.jar $CODE_DIR/target/docker/${IMAGE} 
+cp -r $CODE_DIR/docker $CODE_DIR/target/docker/
+cp -r $CODE_DIR/target/*.jar $CODE_DIR/target/docker/${IMAGE}
 
 REPO=${GROUP}/${IMAGE}
     $DOCKER_CMD build -t ${REPO}:${COMMIT} $CODE_DIR/target/docker/${IMAGE};
